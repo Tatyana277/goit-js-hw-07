87adcf9a646e8238634aa8fd2aefb1221d51ce38
Tatyana277 <tata.nikulina77@gmail.com>
2023-08-18 12:46:56 +0300

    hw07 task 1and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,78 +1,59 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const galleryRef = document.querySelector('.gallery');
 
-galleryRef.addEventListener('click', onImageClick);
-
-// ============================== create markup ==================================
-
-function createImagesMarkup(items) {
-    return items.map(item => 
-    `<div class="gallery__item">
-    <a class="gallery__link" href="${item.original}">
-    <img
-    class="gallery__image"
-    src="${item.preview}"
-    data-source="${item.original}"
-    alt="${item.description}"
-    />
-    </a>
-</div>`
-    ).join('')
-};
-
-const insertMarkup = createImagesMarkup(galleryItems);
-
-galleryRef.innerHTML = insertMarkup;
-
-// ============================== remove listener =================================
-
-function closeModal(e) {
-        if (e.code === 'Escape') {
-            instance.close();
-            window.removeEventListener('keydown', closeModal);
-            console.log(e); //only for check
-        };
-};
-
-// ============================== add lightbox ====================================
-
-const instance = basicLightbox.create(`
-    <img
-    class="gallery__image-open"    
-    src=""
-    alt=""
-    data-source=""
-    />
-    `,
-        {
-            onShow: (instance) => {
-                window.addEventListener('keydown', closeModal);
-            },
-        },
-        {
-            onClose: (instance) => {
-                window.removeEventListener('keydown', closeModal);
-            }
-    },
-    );
-
-// ============================== make click ====================================
-
-function onImageClick(e) {
-    e.preventDefault();
-
-    if (e.target.className !== 'gallery__image') {return};
-
-    const selectedImage = e.target.dataset.source;//${selectedImage}
-    const selectedAlt = e.target.attributes.alt.nodeValue;//${selectedAlt}
-    const minImage = e.target.attributes.src.nodeValue;//${minImage}
-
-    instance.element().querySelector('img').src = selectedImage;
-    instance.element().querySelector('img').alt = selectedAlt;
-    instance.element().querySelector('img').dataSource = minImage;
-
-    instance.show();
-    console.log(e);  //only for check
-};
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+const renderList = (arr, container) => {
+  const markup = arr
+    .map(
+      (item) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+
+  container.insertAdjacentHTML("beforeend", markup);
+};
+
+renderList(galleryItems, listEl);
+
+listEl.addEventListener("click", handleListClick);
+
+function handleListClick(event) {
+  event.preventDefault();
+  if (event.target.className !== 'gallery__image') {
+    return;
+  }
+    
+  const currentPicture = event.target.dataset.source;
+  const currentAltPicture = event.target.alt;
+
+  const modalInstance = basicLightbox.create(`<div class="modal">
+    <img    
+      class="gallery__image"
+      src="${currentPicture}"
+      alt="${currentAltPicture}"
+    />
+  </div>`);
+
+  modalInstance.show();
+
+document.addEventListener('keydown', onClickEsc);
+
+  function onClickEsc(event) {
+    if (event.code === `Escape`) {
+      modalInstance.close();
+      document.removeEventListener('keydown', onClickEsc);
+    }
+  }
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,34 +1,63 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-const galleryRef = document.querySelector('.gallery');
-
-galleryRef.addEventListener('click', onImageClick);
-
-function createImagesMarkup(items) {
-    return items.map(item => 
-    `<a class="gallery__item" href="${item.original}">
-    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-</a>`
-    ).join('')
-};
-
-const insertMarkup = createImagesMarkup(galleryItems);
-
-galleryRef.innerHTML = insertMarkup;
-
-function onImageClick(e) {
-    e.preventDefault();
-    if (e.target.className !== 'gallery__image') { return };
-    
-    new SimpleLightbox('.gallery__item', {
-        captions: true,
-        captionSelector: 'img',
-        captionType: 'attr',
-        captionsData: 'alt',
-        captionPosition: 'bottom',
-        captionDelay: 250,
-    });
-};
 console.log(galleryItems);
 
+const listEl = document.querySelector(".gallery");
+
+const renderList = (arr, container) => {
+  const markup = arr
+    .map(
+      (item) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+    )
+      .join("");
+    
+container.insertAdjacentHTML("beforeend", markup);
+};
+
+renderList(galleryItems, listEl);
+
+ let lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionPosition: 'bottom',
+      captionDelay: 250,
+    })
+
+// listEl.addEventListener("click", handleListClick);
+
+// function handleListClick(event) {
+//   event.preventDefault();
+//   if (event.target.className !== 'gallery__image') {
+//     return;
+//   }
+    
+//   const currentPicture = event.target.dataset.source;
+//   const currentAltPicture = event.target.alt;
+
+//   const modalInstance = basicLightbox.create(`<div class="modal">
+//     <img    
+//       class="gallery__image"
+//       src="${currentPicture}"
+//       alt="${currentAltPicture}"
+//     />
+//   </div>`);
+
+//   modalInstance.show();
+
+// document.addEventListener('keydown', onClickEsc);
+
+//   function onClickEsc(event) {
+//     if (event.code === `Escape`) {
+//       modalInstance.close();
+//       document.removeEventListener('keydown', onClickEsc);
+//     }
+//   }
+// };
